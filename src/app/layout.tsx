58e9019182d0bd869cf5a330a3f961c299e0fc86
HForGames <hugo.galan8@gmail.com>
2025-04-29 11:04:37 +0200

    fixing layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,16 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <>
         <head>
             <title>JSCMP</title>
-            <!-- Primary Meta Tags -->
             <title>JS-CMP – JavaScript to C++ Transpiler</title>
             <meta name="title" content="JS-CMP – JavaScript to C++ Transpiler"/>
             <meta name="description"
                   content="JS-CMP is a transpiler that converts JavaScript code into C++, compiling it into high-performance binaries. Supports Linux and macOS."/>
 
-            <!-- Favicon -->
             <link rel="icon" type="image/png" href="eclair.png"/>
 
-            <!-- Open Graph / Facebook -->
             <meta property="og:type" content="website"/>
             <meta property="og:url" content="https://github.com/JS-CMP/web/"/>
             <meta property="og:title" content="JS-CMP – JavaScript to C++ Transpiler"/>
@@ -27,7 +24,6 @@ export default function RootLayout({
                   content="Convert JavaScript to high-performance C++ executables. Supports Linux and macOS."/>
             <meta property="og:image" content="eclair.png"/>
 
-            <!-- Twitter -->
             <meta property="twitter:card" content="summary"/>
             <meta property="twitter:url" content="https://github.com/JS-CMP/web/"/>
             <meta property="twitter:title"
@@ -37,16 +33,14 @@ export default function RootLayout({
             <meta property="twitter:image" content="eclair.png"/>
 
             <script defer data-domain="github.com/js-cmp/web"
-                    src="https://plausible.hgalan.dev/js/script.file-downloads.outbound-links.js"></script>
-            <script>window.plausible = window.plausible ||
-                function() {(window.plausible.q = window.plausible.q || []).push(arguments)}</script>
+                    src="https://plausible.hgalan.dev/js/script.file-downloads.js"></script>
         </head>
-        <body>
-        <Header/>
-        <div className="px-28">
-            {children}
-        </div>
-        </body>
-        </html>
+            <body>
+            <Header/>
+            <div className="px-28">
+                {children}
+            </div>
+            </body>
+        </>
     )
 }
